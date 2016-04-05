d5155ebc01bb4de1fd6a4280c8d350b59316f84e
tript <trongtri92nd@gmail.com>
2016-04-05 23:40:49 +0700

    update user/all/edit page

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,5 @@
 class School < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :name, use: :slugged
   has_and_belongs_to_many :active_classes, join_table: "school_has_classes", foreign_key: "school_id", association_foreign_key: "class_id"
 end
