c01d459f25aca36ee085aae071d6d37e08e50ba2
tript <trongtri92nd@gmail.com>
2016-06-16 19:58:33 +0700

    fix bug: display type lesson in so gd-dt

--- app/controllers/type_controller.rb
+++ app/controllers/type_controller.rb
@@ -16,7 +16,20 @@ class TypeController < ApplicationController
           class_lesson.lessons = lessons
           @class_lessons.push(class_lesson)
         end
-      elsif @school.grade_id != 7
+      elsif @school.grade_id == 7
+        @classes = ActiveClass.all.order('id ASC')
+        @class_lessons = Array.new
+        @type_id = params[:type_id]
+        @classes.each do |active_class|
+          class_lesson = ClassLesson.new
+          class_lesson.class = active_class
+          quantity = Lesson.joins(:user).where(class_id: active_class.id, type_id: @type_id).size
+          lessons = Lesson.joins(:user).where(class_id: active_class.id, type_id: @type_id).limit(10)
+          class_lesson.quantity = quantity
+          class_lesson.lessons = lessons
+          @class_lessons.push(class_lesson)
+        end
+      else
         @classes = @school.active_classes
         @class_lessons = Array.new
         @type_id = params[:type_id]
